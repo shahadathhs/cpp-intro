1378bcbe4cd2e31942c3193080dc4f48a4102ff2
shahadathhs <shahadathhossensajib732@gmail.com>
2025-11-22 05:33:36 +0600

    feat: sort it problem solved

---
+++ Sort_It.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    int arr[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + N);
+    
+    for (int i = 0; i < N; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+    
+    sort(arr, arr + N, greater<int>());
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
