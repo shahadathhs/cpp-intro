4c38a2d4fd4297c85dcad40698f6d72361715aeb
shahadathhs <shahadathhossensajib732@gmail.com>
2025-12-03 21:20:01 +0600

    feat: finde jessica

--- Find_Jessica.cpp
+++ Find_Jessica.cpp
@@ -5,7 +5,30 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    string S;
+    getline(cin, S);
+
+    stringstream ss(S);
+    string word;
+    bool found = false;
+
+    while (ss >> word) 
+    {
+        if (word == "Jessica") 
+        {
+            found = true;
+            break;
+        }
+    }
+
+    if (found) 
+    {
+        cout << "YES\n";
+    } 
+    else 
+    {
+        cout << "NO\n";
+    }
 
     return 0;
 }
